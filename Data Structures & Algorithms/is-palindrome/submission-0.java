cfc4c6d4ea7abc8f856905a3aeed5b99a07ebf53
BlackBird-SR-71 <kevbhikadia@gmail.com>
2026-03-29 18:35:05 +0100

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String temp = s;
+
+        int l = 0;
+        int r = temp.length()-1;
+        if (temp.length() == 0) {
+            return true;
+        }
+
+        while (l < r) {
+            char left  = temp.charAt(l);
+            char right  = temp.charAt(r);
+
+            
+
+            if (!Character.isLetterOrDigit(left)) {
+                l++;
+                continue;
+            }
+
+            if (!Character.isLetterOrDigit(right)) {
+                r--;
+                continue;
+                
+            }
+            if (Character.toLowerCase(temp.charAt(l)) == Character.toLowerCase(temp.charAt(r))) {
+                l++;
+                r--;
+            }
+            else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
