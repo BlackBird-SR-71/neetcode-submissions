7c61c1aadc95f3402fd3038cf85528ff719016b1
BlackBird-SR-71 <kevbhikadia@gmail.com>
2026-04-06 21:42:36 +0100

    Add: count-connected-components - submission-5

---
+++ Data Structures & Algorithms/count-connected-components/submission-5.java
@@ -0,0 +1,68 @@
+class Solution {
+
+
+
+
+    class Dsu {
+        int comp;
+        int[] parent;
+        int[] size;
+
+        public Dsu(int n) {
+            comp = n;
+            parent = new int[n];
+            size = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public boolean union(int u, int v) {
+            int pu = find(u);
+            int pv = find(v);
+
+            if (pu == pv) {
+                return false;
+            }
+
+            if (size[pu] < size[pv]) {
+                int temp = pu;
+                pu = pv;
+                pv = temp;
+            }
+
+            comp--;
+            size[pu] += size[pv];
+
+            parent[pv] = pu;
+
+            return true;
+        }
+
+        public int find(int num) {
+            if (num != parent[num]) {
+                parent[num] = find(parent[num]);
+            }
+            return parent[num];
+        }
+
+    //     public int find(int node) {
+    //     if (Parent[node] != node) {
+    //         Parent[node] = find(Parent[node]);
+    //     }
+    //     return Parent[node];
+    // }
+        
+    }
+    public int countComponents(int n, int[][] edges) {
+
+        Dsu dsu = new Dsu(n);
+        for (int[] edge : edges) {
+            dsu.union(edge[0], edge[1]);
+
+        }
+
+        return dsu.comp;
+    }
+}
