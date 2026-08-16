4606ddb26ac9636216f056ba85565e8622fda71c
BlackBird-SR-71 <kevbhikadia@gmail.com>
2026-08-16 19:56:29 +0100

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int l = 1;
+        int r = Integer.MIN_VALUE;
+
+        for (int i : piles) {
+            r = Math.max(r, i);
+        }
+        int res = r;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+            int hours = 0;
+            for (int ban : piles) {
+                hours += Math.ceil((ban * 1.0) / m); 
+            }
+            
+            if (hours <= h) {
+                r = m - 1;
+                res = Math.min(res, m);
+            }
+            else if (hours > h) {
+                l = m + 1;
+            }
+        }
+
+        return res;
+    }
+}
